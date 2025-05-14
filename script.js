02f0acdede2a342a6ad5a0f1e2b04b829e8fae06
Jhon Ramirez <jhramirez9716@gmail.com>
2025-05-13 22:07:13 -0500

    JS

---
+++ script.js
@@ -0,0 +1,80 @@
+"use strict";
+/*CONTROL DE FLUJO*/
+
+//SECUENCIA
+/*
+console.log("1...")
+console.log("2...")
+console.log("3...")*/
+
+// CONDICIONALES = CUMPLE O NO CUMPLE
+// se utilizan valores bulianos
+// IF = SI (true) cumple
+// ELSE = No cumple
+// if se puede utilizar solo o con else
+// else no se puede utilizar solo
+/*
+miVariable1 = true;
+
+
+if (miVariable1){
+    console.log("si se cumplio la condición");
+}
+if (miVariable1){
+   console.log("si se cumplio la condición");
+} else {
+console.log("no se cumplio la condición")
+}
+console.log("hola") */
+
+// Si es pepita, saludela con un Hola, si es Pepito saludelo con un Hello, de otra manera salude a un desconocido
+/*
+const nombre = "Anne"
+
+if (nombre === "pepita"){
+        console.log("Hola, pepita")
+} 
+if (nombre === "pepito"){
+        console.log("Hello, pepito")
+}else {
+        console.log("Hola, desconocido 2")
+}
+*/
+
+/* Crear una función que reciba una palabra
+- si la palabra es "hola", imprimir por consola "HOLA"
+*/
+
+/*
+
+miVariable = 1;
+*/
+//Estructura de control
+//switch
+/*
+switch (miVariable) {
+        case 1:
+                console.log("soy 1")
+                break;
+        case 2:
+                console.log("soy 2")
+                break;
+        default:
+                console.log("no se cumplio ningún caso");
+                break;
+}
+*/
+
+/* Crear una función que reciba una palabra
+- si la palabra es "hola", imprimir por consola "HOLA"
+crear 2 funciones, una debe utilizar dentro if y la otra debe usar switch
+*/
+
+/* Crear una función con 3 parametros
+1. va a recibir un numero
+2. va a recibir un otro numero
+3. recibir una palabra
+mostrar el resultado de las operaciones aritmeticas basicas según llamado
+suma, resta, multiplicación y división
+*/
+
